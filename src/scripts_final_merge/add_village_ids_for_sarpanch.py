1b746d5eea110ec4cb5c2d26ee8dcc65cdb23b31
polofr <paulhenri.carton@gmail.com>
2022-01-08 18:25:51 -0800

    Sarpanch_Survey_1.csv updated

--- src/scripts_final_merge/add_village_ids_for_sarpanch.py
+++ src/scripts_final_merge/add_village_ids_for_sarpanch.py
@@ -4,6 +4,9 @@ import sys
 import csv
 import json
 
+from src.scripts_final_merge.utils.sampling_village_ids import SamplingVillageIds
+from src.util.csv_writer import CsvWriter
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
 
 
@@ -69,7 +72,10 @@ def main(argv):
                     instanceid_column_pos = find_column_position(line, 'instanceid')
                     continue
                 instanceid = line[instanceid_column_pos]
-                if instanceid == '':
+                if not instanceid:
+                    continue
+                if not line[villageid_column_pos]:
+                    # print(f'missing village id for {instanceid} inside {file_with_ids}')
                     continue
                 if instanceid in instanceid_set:
                     # print(f'Duplicate {instanceid} inside Sarpanch_Group_merged.csv')
@@ -80,12 +86,15 @@ def main(argv):
                     'villageid': line[villageid_column_pos].split('.0')[0]
                 }
 
+    SamplingVillageIds.prepare()
+
     file_suffixes = ['1', '2', '2_bis', '3', '4']
     for file_suffix in file_suffixes:
         file_path = f'C:/Data_PoloFr/scrap-python-indian-gov/src/scripts_final_merge/csv_files/Sarpanch_Survey_{file_suffix}.csv'
         if not os.path.isfile(file_path):
             raise Exception(f'{file_path} is not valid ')
         try:
+            result_lines = []
             with open(file_path, 'r', encoding='utf-8') as original:
                 lines = csv.reader(original, delimiter=',')
                 skip_first = True
@@ -93,7 +102,8 @@ def main(argv):
                 villageid_column_pos = None
                 villagename_column_pos = None
                 instanceid_column_pos = None
-                for line in lines:
+                for idx, line in enumerate(lines):
+                    result_lines.append(line)
                     if skip_first is True:
                         skip_first = False
                         district_column_pos = find_column_position(line, district_column_name)
@@ -110,7 +120,8 @@ def main(argv):
 
                     result = instanceid_set.get(instanceid)
                     if result is None:
-                        print(f'Could not find a village id in Sarpanch_Survey_{file_suffix}.csv for {instanceid} {district} {villagename}')
+                        print(f'Could not find a village id at line {idx + 1} in Sarpanch_Survey_{file_suffix}.csv for {instanceid} {district} {villagename}')
+                        SamplingVillageIds.find_best_match(villagename, district)
                     else:
                         expected_result = {
                             'district': district,
@@ -119,6 +130,10 @@ def main(argv):
                         }
                         if result != expected_result:
                             print(f'Found a village id for {instanceid} in Sarpanch_Survey_{file_suffix}.csv but {json.dumps(result)} vs {json.dumps(expected_result)}')
+                        result_lines[-1][villageid_column_pos] = result['villageid']
+                        result_lines[-1][villageid_column_pos + 1] = result['villageid']
+
+                CsvWriter.write(file_path.replace('csv_files', 'csv_files_corrected'), result_lines)
         except Exception as exp:
             raise Exception(f'Failed for Sarpanch_Survey_{file_suffix}.csv : {str(exp)}')
 
