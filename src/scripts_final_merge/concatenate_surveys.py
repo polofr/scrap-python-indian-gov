e0e97f0c4ad97acac76cc2c4452a8b808314ca4c
polofr <paulhenri.carton@gmail.com>
2022-01-11 12:46:18 -0800

    concatenate_surveys.py

---
+++ src/scripts_final_merge/concatenate_surveys.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import os
+import sys
+import pandas as pd
+
+
+def print_usage(executable_name, error_msg=None):
+    print(f'Usage: {executable_name}')
+    print()
+    if error_msg is not None:
+        print(f'Error: {error_msg}')
+    sys.exit(1)
+
+
+def main(argv):
+    file_prefixes = ['Gram_Sevak_Survey_', 'Group_Survey_', 'Notable_Survey_', 'Sarpanch_Survey_', 'Upa_Sarpanch_Survey_']
+    for file_prefix in file_prefixes:
+        file_suffixes = ['1', '2', '2_bis', '3', '4']
+        all_dataframes = []
+        for file_suffix in file_suffixes:
+            file_path = f'C:/Data_PoloFr/scrap-python-indian-gov/src/scripts_final_merge/csv_files_corrected/{file_prefix}{file_suffix}.csv'
+            if not os.path.isfile(file_path):
+                raise Exception(f'{file_path} is not valid ')
+            try:
+                df = pd.read_csv(file_path, dtype=str) # nrows=5,
+                all_dataframes.append(df)
+            except Exception as exp:
+                raise Exception(f'Failed for {file_path}: {str(exp)}')
+        final_dataframe = pd.concat(all_dataframes, axis=0, ignore_index=True)
+        file_path = f'C:/Data_PoloFr/scrap-python-indian-gov/src/scripts_final_merge/csv_files_merged/{file_prefix}.csv'
+        final_dataframe.to_csv(path_or_buf=file_path, index=False)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
