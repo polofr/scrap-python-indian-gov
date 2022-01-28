7ef1706040633c05dc33debd5b5f0630ed9e91a1
polofr <paulhenri.carton@gmail.com>
2022-01-28 15:01:06 -0800

    pushing new results with district code and names at the beginning of the line

--- src/scripts_state_election_commission/merge_all_csv.py
+++ src/scripts_state_election_commission/merge_all_csv.py
@@ -3,6 +3,8 @@ import sys
 from os import listdir
 from os.path import join, isfile
 
+from src.scripts_state_election_commission.config.districts import DISTRICTS
+
 
 def main(argv):
     new_csv_lines = []
@@ -13,6 +15,7 @@ def main(argv):
             continue
         district_number = f.split('.')[0]
         district_number = district_number.lstrip('0')
+        district_name = DISTRICTS[district_number]
         with open(file_path, 'r', encoding='utf8') as to_append:
             lines = to_append.readlines()
             is_first = True
@@ -20,8 +23,8 @@ def main(argv):
                 if is_first is True:
                     is_first = False
                     continue
-                new_csv_lines.append(f'{district_number},{line}')
-    column_names = 'district,ब्लॉक,ग्राम पंचायत,पद का आरक्षण,उम्मीदवार,पिता/पति,प्रत्याशी का आरक्षण,शैक्षिक योग्यता,लिंग,मोबाइल नं०,प्राप्त वैध मत,प्राप्त मत %,मतदान %,परिणाम'
+                new_csv_lines.append(f'{district_number},{district_name},{line}')
+    column_names = 'district_id,district,ब्लॉक,ग्राम पंचायत,पद का आरक्षण,उम्मीदवार,पिता/पति,प्रत्याशी का आरक्षण,शैक्षिक योग्यता,लिंग,मोबाइल नं०,प्राप्त वैध मत,प्राप्त मत %,मतदान %,परिणाम'
     new_path = 'C:/Data_PoloFr/scrap-python-indian-gov/src/scripts_state_election_commission/merged_results.csv'
     with open(new_path, 'w', encoding='utf8') as result_file:
         result_file.writelines([column_names])
