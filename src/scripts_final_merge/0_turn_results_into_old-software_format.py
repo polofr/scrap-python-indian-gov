a4642d8102101f7800fad954cdc19b419d7100c5
polofr <paulhenri.carton@gmail.com>
2022-08-24 17:55:13 -0700

    0_turn_results_into_old-software_format.py 2

--- src/scripts_final_merge/0_turn_results_into_old-software_format.py
+++ src/scripts_final_merge/0_turn_results_into_old-software_format.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 import os
 import re
-import sys
 import pandas as pd
 
 from src.scripts_final_merge.utils.columns import COLUMNS
 
 
-def main(argv):
-    file_path = 'C:/Data_PoloFr/scrap-python-indian-gov/csv_files/ahmednagar/Gram_Sevak_Survey_WIDE.csv'
+def turn_into_old_format(file_path, new_file_path):
     if not os.path.isfile(file_path):
         raise Exception(f'{file_path} is not valid')
     df = pd.read_csv(file_path, dtype=str)
     expected_columns = [c for c in df.columns]
     print(f'Expecting {len(expected_columns)} columns')
-    print(sorted(expected_columns))
+    print(expected_columns)
     print()
 
     file_path = 'C:/Data_PoloFr/scrap-python-indian-gov/csv_files/amravati/Gram_Sevak_Survey_Amravati_-_all_versions_-_False_-_2022-07-29-05.csv'
@@ -55,6 +53,7 @@ def main(argv):
             new_column = old_column.replace('/', '_')
             if new_column in df.columns:
                 print(f'column {new_column} already exists 2')
+                continue  # TODO
             df = df.rename({old_column: new_column}, axis=1)
             continue
 
@@ -124,9 +123,24 @@ def main(argv):
     df['Q205_4'] = ''
     df['formdef_version'] = ''
 
+    # TODO
+    df = df.drop('Q22/1', axis=1)
+    df = df.drop('Q22/2', axis=1)
+    df = df.drop('Q22/3', axis=1)
+    df = df.drop('Q22/4', axis=1)
+    df = df.drop('Q22/5', axis=1)
+    df = df.drop('Q22/6', axis=1)
+    df = df.drop('Q22/7', axis=1)
+    df = df.drop('Q22/8', axis=1)
+    df = df.drop('Q22/9', axis=1)
+    df = df.drop('Q22/10', axis=1)
+    df = df.drop('Q50_1/1', axis=1)
+
+    df = df.reindex(columns=expected_columns)
+
     current_columns = [c for c in df.columns]
     print(f'Currently {len(current_columns)} columns')
-    print(sorted(current_columns))
+    print(current_columns)
 
     print('missing matches for')
     for column in df.columns:
@@ -137,10 +151,21 @@ def main(argv):
         if column not in df.columns:
             print(f"df['{column}'] = ''")
 
-    df = df.reindex(columns=expected_columns)
-    file_path = f'C:/Data_PoloFr/scrap-python-indian-gov/src/scripts_final_merge/csv_files/Gram_Sevak_Survey_5.csv'
-    df.to_csv(path_or_buf=file_path, index=False)
+    if expected_columns != current_columns:
+        raise Exception('Columns are not matching')
+
+    df.to_csv(path_or_buf=new_file_path, index=False)
+
+
+def main():
+    file_path = 'C:/Data_PoloFr/scrap-python-indian-gov/csv_files/ahmednagar/Gram_Sevak_Survey_WIDE.csv'
+    new_file_path = f'C:/Data_PoloFr/scrap-python-indian-gov/src/scripts_final_merge/csv_files/Gram_Sevak_Survey_5.csv'
+    turn_into_old_format(file_path, new_file_path)
+
+    file_path = 'C:/Data_PoloFr/scrap-python-indian-gov/csv_files/amravati/Gram_Sevak_Survey_Amravati_-_all_versions_-_False_-_2022-07-29-05.csv'
+    new_file_path = f'C:/Data_PoloFr/scrap-python-indian-gov/src/scripts_final_merge/csv_files/Gram_Sevak_Survey_6.csv'
+    turn_into_old_format(file_path, new_file_path)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
