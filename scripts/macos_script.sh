52df7c361fb6b0281618ceb9e0a838056049dad2
polofr <paulhenri.carton@gmail.com>
2023-07-19 21:56:50 -0700

    initial results

---
+++ scripts/macos_script.sh
@@ -0,0 +1,11 @@
+cd /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/results/
+source /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/venv/bin/activate
+python3 /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/src/scripts/1_get_list_of_districts.py 2021 2021-2022 UTTAR PRADESH
+
+cd /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/results/
+source /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/venv/bin/activate
+python3 /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/src/scripts/1_get_list_of_districts.py 2020 2020-2021 UTTAR PRADESH
+
+cd /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/results/
+source /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/venv/bin/activate
+python3 /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/src/scripts/1_get_list_of_districts.py 2019 2019-2020 UTTAR PRADESH
