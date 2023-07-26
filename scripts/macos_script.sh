8f79352f73828711fd18a31567e89c1e9687f7e9
polofr <paulhenri.carton@gmail.com>
2023-07-26 06:45:58 -0700

    fix the code by using latest web manager, add results_UTTAR PRADESH_2021.csv

--- scripts/macos_script.sh
+++ scripts/macos_script.sh
@@ -1,6 +1,6 @@
 cd /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/
 source ./venv/bin/activate
-python3 ./scripts/2_get_missing_entries.py 2021 2021-2022 UTTAR PRADESH
+python3 ./src/scripts/2_get_missing_entries.py 2021 2021-2022 UTTAR PRADESH
 
 cd /Users/paulhenricarton/polofr_repos/scrap-python-indian-gov/
 source ./venv/bin/activate
